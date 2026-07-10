a000ce06dca460de3c5c999d5876ad5f0dc9a978
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-10 10:34:44 +0000

    Jul 10, 2026, 10:34 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -21,7 +21,7 @@ circle(260,255,30)
 circle(340,255,30)
 ellipse(300,310,35,17)
   // Challenge 3: A house using rect() function
-
+rect
 
   // Challenge 4: A rocketship using triangle() function
 
