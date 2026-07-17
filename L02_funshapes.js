f43be2043e38f5b399e4fc110282bf993a06cb97
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-17 10:03:20 +0000

    Jul 17, 2026, 10:03 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -43,7 +43,7 @@ function draw() {
   // Challenge 7: Create a funny Pokemon
 circle(400,400,200)
 ellipse(400,470,150,75)
-rect(200,200,150,150,6,200,4,200)
+rect(200,200,150,150,6,300,4,300)
 
 
 }
