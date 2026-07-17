e12363f38e8bf81505ca8aea15ce69faf8bc2440
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-17 10:44:13 +0000

    Jul 17, 2026, 10:44 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -8,7 +8,7 @@ function draw() {
   // Recap 1: Repeating Circles
 let diameter = 50;
 
-let circley
+let circley = 1
 for(let count = 1 ; count<6 ; count++){
   circley = circley +50
   circle(count,circley,100)
