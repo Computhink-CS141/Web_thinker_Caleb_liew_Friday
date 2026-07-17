afac3e3c13af3115c5657c3aca07043b096600db
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-17 10:44:37 +0000

    Jul 17, 2026, 10:44 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -10,7 +10,7 @@ let diameter = 50;
 
 let circley = 1
 for(let count = 1 ; count<6 ; count++){
-  circley = circley +50
+  circley = circley +1
   circle(count,circley,100)
 }
 // circle(225,200,100)
