ceaf108f45a4aec66fa0c7e09048998f4f21f87f
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-17 10:37:56 +0000

    Jul 17, 2026, 10:37 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -9,7 +9,7 @@ function draw() {
 let diameter = 50;
 
 
-for(let count = 1 ; count<6; count++){
+for(let count = 1, circley = 1 ; count<6; count++){
   circle(75,200,100)
   circle(225,200,100)
   circle(375,200,100)
