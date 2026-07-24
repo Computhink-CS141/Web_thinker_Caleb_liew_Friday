78cde38a583ea131721e6652b39eda11576b87be
Computhink-CS141 <cs141@computhink.com.sg>
2026-07-24 10:49:54 +0000

    Jul 24, 2026, 10:49 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -41,7 +41,7 @@ circle(width/2,300,300)
     // shapeColor= color(random(0,255),random(0,255),random(0,255))
     // circle(mouseX,mouseY,50)
 // }
-
+let size =0
 function mouseDragged(){
     // shapeColor= color(random(0,255),random(0,255),random(0,255))
     circle(mouseX,mouseY,50)
