6ffc2606e9675622f37e8bd4bf27d5cdbb2ecbf2
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-07 10:18:06 +0000

    Aug 7, 2026, 10:18 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -12,26 +12,24 @@ function setup(){
     xpos = width/2;
     ypos = height/2;
 }
-
-
-function draw(){
-    background("skyblue");
-    fill("white");
-    rect(width/2,height/2,300,300)
-    fill("black");
-    rect(xpos,ypos,50,50)
-    if (keyIsDown(UP_ARROW)){
-        ypos = ypos-3
-    }
-    if (keyIsDown(DOWN_ARROW)){
-        ypos = ypos+3
-    }
-    if (keyIsDown(RIGHT_ARROW)){
-        xpos = xpos+3
-    }
-    if (keyIsDown(LEFT_ARROW)){
-        xpos = xpos-3
-    }
-    xpos = constrain(xpos,75,325);
-    ypos = constrain(ypos,75,325);
-}
+// function draw(){
+//     background("skyblue");
+//     fill("white");
+//     rect(width/2,height/2,300,300)
+//     fill("black");
+//     rect(xpos,ypos,50,50)
+//     if (keyIsDown(UP_ARROW)){
+//         ypos = ypos-3
+//     }
+//     if (keyIsDown(DOWN_ARROW)){
+//         ypos = ypos+3
+//     }
+//     if (keyIsDown(RIGHT_ARROW)){
+//         xpos = xpos+3
+//     }
+//     if (keyIsDown(LEFT_ARROW)){
+//         xpos = xpos-3
+//     }
+//     xpos = constrain(xpos,75,325);
+//     ypos = constrain(ypos,75,325);
+// }
