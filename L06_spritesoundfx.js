f541c55c1e5a4bc950f0d99a3f97098589a6363a
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-07 09:53:49 +0000

    Aug 7, 2026, 9:53 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -19,9 +19,9 @@ function draw(){
         ypos = ypos+1
     }
     if (keyIsDown(DOWN_ARROW)){
-        ypos = ypos+1
+        ypos = ypos-1
     }
-    if (keyIsDown(UP_ARROW)){
+    if (keyIsDown(RIGHT_ARROW)){
         ypos = ypos+1
     }
     if (keyIsDown(UP_ARROW)){
