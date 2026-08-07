0c29072eb469d34fec080fc1718757b81a7aa174
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-07 10:46:12 +0000

    Aug 7, 2026, 10:46 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -41,7 +41,7 @@ function draw(){
 function keyPressed(){
     if (keyCode === 32);
     soundSFX.play();
-    if (keyCode === 32);
+    if (keyCode (UP_ARROW));
     soundSFX.play();
     if (keyCode === 32);
     soundSFX.play();
