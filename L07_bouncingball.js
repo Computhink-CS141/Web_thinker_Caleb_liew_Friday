4bfbf109bd4b5b2bc932e6ef52a5784d94e25353
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-14 10:46:41 +0000

    Aug 14, 2026, 10:46 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -26,6 +26,7 @@ let randomR,randomG,randomB;
 
 function draw(){
     // background (0);
+    fill(255)
     circle (ballx,bally,ballSize)
     ballx = ballx + ballSpeedx;
     bally = bally + ballSpeedy;
