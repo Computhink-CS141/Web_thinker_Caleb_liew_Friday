8db52c68b9ae521fbfdd17945bf70c42996d062b
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-14 09:54:12 +0000

    Aug 14, 2026, 9:54 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -9,12 +9,12 @@ function preload(){
 function setup(){
     createCanvas(500,500);
     background(0);
-    imageMode(C)
+    imageMode(CENTER);
 }
 
 function draw(){
     background(0);
-
+    
 
 
 }
