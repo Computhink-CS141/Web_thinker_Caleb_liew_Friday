25bdf026d7b4ebe338b5b246d027d598821d950f
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-14 09:59:22 +0000

    Aug 14, 2026, 9:59 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -17,6 +17,16 @@ function setup(){
 function draw(){
     background(0);
     image(picoimg,xpos,ypos,widthsize,133);
-    
-
+    if (keyIsDown(RIGHT_ARROW)){
+        xpos = xpos+3
+    }
+    if (keyIsDown(LEFT_ARROW)){
+        xpos = xpos-3
+    }
+    if (keyIsDown(UP_ARROW)){
+        ypos = ypos-3
+    }
+    if (keyIsDown(DOWN_ARROW)){
+        ypos = ypos+3
+}
 }
