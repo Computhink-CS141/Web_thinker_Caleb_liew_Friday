7860c966a078b13fb7489b78b4287c7cf86f0301
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-14 10:57:47 +0000

    Aug 14, 2026, 10:57 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -61,7 +61,7 @@ function draw(){
 }
 function keyPressed(){
     if(keyCode === UP_ARROW){
-        ballSpeedx*=10;
+        ballSpeedx *=10;
         ballSpeedy *=10;
     } else if(keyCode === DOWN_ARROW){
         ballSpeedx *=0.8;
