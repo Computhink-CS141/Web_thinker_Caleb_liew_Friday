1766537fdd353490ecbc0fe5451359dbc238e0d4
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-14 10:48:00 +0000

    Aug 14, 2026, 10:48 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -56,7 +56,7 @@ function draw(){
         randomR = random(0,255);
         randomB = random(0,255);
         randomG = random(0,255);
-        fill(randomR,randomB,randomG,);
+        fill(randomR,randomB,randomG,random(0,255));
     }
 
 
