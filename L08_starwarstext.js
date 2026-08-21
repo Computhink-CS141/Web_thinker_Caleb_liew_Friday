308eaa5e77e28458e370b1e28e8467d8e5a92489
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-21 10:37:48 +0000

    Aug 21, 2026, 10:37 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -11,6 +11,6 @@ function setup(){
 
 function draw(){
     background("black")
-    let storytext=["A long time ago in a Galaxy" ]
+    let storytext=["A long time ago in a Galaxy far,","far" ]
 
 }
