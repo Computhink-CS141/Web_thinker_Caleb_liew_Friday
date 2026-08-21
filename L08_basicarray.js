00dde481168543c2c02d567a085480522cd06c72
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-21 10:00:48 +0000

    Aug 21, 2026, 10:00 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,11 +6,11 @@ function setup(){
 
 function draw(){
 background("grey");
-fill("white");
-textSize();
-text("My name is Caleb.",50,50);
-text("I am 12 years old this year.",50,100);
-text("My favourite activity is reading.",50,150);
+// fill("white");
+// textSize();
+// text("My name is Caleb.",50,50);
+// text("I am 12 years old this year.",50,100);
+// text("My favourite activity is reading.",50,150);
 
 }
 
