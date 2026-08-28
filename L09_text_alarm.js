dfc903d939ce21b21559ecef97e29be525d610d4
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-28 10:21:25 +0000

    Aug 28, 2026, 10:21 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,21 +1,21 @@
 
 
-function setup(){
-    createCanvas(500,500);
-    background("skyblue");
-    textAlign(CENTER,CENTER);
-}
+// function setup(){
+//     createCanvas(500,500);
+//     background("skyblue");
+//     textAlign(CENTER,CENTER);
+// }
 
-function draw(){
-    background("skyblue");
-    let xhour = nf(hour(),2);
-    let xminute = nf(minute(),2);
-    let xsecond = nf(second(),2);
-    let timeString = xhour + ":" + xminute + ":" + xsecond;
-    textSize(50);
-    text(timeString,width/2,height/2);
+// function draw(){
+//     background("skyblue");
+//     let xhour = nf(hour(),2);
+//     let xminute = nf(minute(),2);
+//     let xsecond = nf(second(),2);
+//     let timeString = xhour + ":" + xminute + ":" + xsecond;
+//     textSize(50);
+//     text(timeString,width/2,height/2);
 
-}
+// }
 
 
 
