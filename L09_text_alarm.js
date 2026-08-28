f4f00373eafac2f01464a1aeca51e79ca2031cfd
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-28 09:47:18 +0000

    Aug 28, 2026, 9:47 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -15,7 +15,7 @@ function draw(){
     text("bounce",xpos,height/2);
     xpos = xpos+speed;
 
-    if (xpos > width){
+    if (xpos > width-45){
         speed = speed*-1;
     }
     if (xpos < 40){
