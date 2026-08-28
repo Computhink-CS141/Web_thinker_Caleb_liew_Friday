f62efec03e834f999ce38118e8a894ccbc30bfa1
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-28 10:08:46 +0000

    Aug 28, 2026, 10:08 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -9,8 +9,8 @@ function setup(){
 function draw(){
     background("skyblue");
     let hour = mf(hour,1,1);
-    let minute = mf(hour,1,1);
-    let second = mf(hour,1,1);
+    let minute = mf(miute,1,1);
+    let second = mf(second,1,1);
     let timeString = "fakeclock";
 
 
