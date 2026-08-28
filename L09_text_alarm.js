4e691a8c4cac80cc7547514791ac92da089255e4
Computhink-CS141 <cs141@computhink.com.sg>
2026-08-28 10:02:57 +0000

    Aug 28, 2026, 10:02 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -5,8 +5,9 @@ function setup(){
     createCanvas(500,500);
     background("grey");
     textAlign(CENTER,CENTER);
-    xpos = width/2;
-    speed = 5;
+    
+    // xpos = width/2;
+    // speed = 5;
 }
 
 function draw(){
@@ -15,7 +16,9 @@ function draw(){
 
 
 
-    
+
+
+
     // Recap 1 -------------------------------------------------------
 
     // textSize(50);
