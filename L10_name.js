1a049fef81c9bf1604ddbe21e573262a31f118b7
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:45:34 +0000

    Sep 11, 2026, 10:45 AM

--- L10_name.js
+++ L10_name.js
@@ -7,7 +7,7 @@ function setup(){
     createCanvas(400,400);
     textAlign(CENTER,CENTER);
     colorpicker = createColorPicker("#8a2727")
-    colorpicker.position(300,300);
+    colorpicker.position(300,360);
     input= createInput("");
     input.position(width/2,300);
     input.input(updateText);
