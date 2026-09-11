fbd753ba3f7232c298dc36d6b47da0c8075a671c
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:43:08 +0000

    Sep 11, 2026, 10:43 AM

--- L10_name.js
+++ L10_name.js
@@ -19,7 +19,7 @@ function setup(){
 
 function draw(){
     background(colorpicker.value());
-    rect(100,100,200,200)
+    rect(100,100,350,200)
     textSize(24);
     text(usertext,width/2,height/2);
     
