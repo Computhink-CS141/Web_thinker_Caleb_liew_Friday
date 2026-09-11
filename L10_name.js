ea44f93f098f8269af0fe0117782f3243da6b8b0
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:57:17 +0000

    Sep 11, 2026, 10:57 AM

--- L10_name.js
+++ L10_name.js
@@ -51,7 +51,7 @@ function draw(){
     text("Rectangle color:",143,400)
 
     textSize(14);
-    text("Text color:",143,450)
+    text("Text color:",143,430)
 }
 function updateText(){
     usertext = this.value();
