e062962ad69e41e57a8b259cfa6fac0232c2472a
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:24:11 +0000

    Sep 11, 2026, 10:24 AM

--- L10_name.js
+++ L10_name.js
@@ -29,7 +29,7 @@ function draw(){
     text("Enter name here:",140,312)
 
     textSize(14);
-    text("Enter age here:",140,342)
+    text("Enter age here:",143,342)
 }
 function updateText(){
     usertext = this.value();
