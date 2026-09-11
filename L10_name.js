6d6a90de061a8cd2f46813522674a43911bda420
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:29:11 +0000

    Sep 11, 2026, 10:29 AM

--- L10_name.js
+++ L10_name.js
@@ -2,7 +2,7 @@
 let colorpicker;
 function setup(){
     createCanvas(400,400);
-    colorpicker = cteatecolorpicker("#8a2727")
+    colorpicker = createcolorpicker("#8a2727")
     colorpicker.postion(300,300);
 }
 
