1ca869c287e69db59ebb88efc8d9dcba17183848
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 10:12:25 +0000

    Sep 11, 2026, 10:12 AM

--- L10_name.js
+++ L10_name.js
@@ -6,7 +6,7 @@ function setup(){
     background(220);
     textAlign(CENTER,CENTER);
     input= createInput("");
-    input.position(300,300);
+    input.position(width/2,300);
 }
 
 function draw(){
