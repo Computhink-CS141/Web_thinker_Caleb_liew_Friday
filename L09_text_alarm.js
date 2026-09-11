bacb7027530a06c6d74f94051e345cacbead9cfe
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-11 09:57:05 +0000

    Sep 11, 2026, 9:57 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -19,7 +19,7 @@ function draw(){
     let xsecond=(second(),2);
     let xminute=(minute(),2);
     let xhour=(hour(),2);
-    let 
+    let timestring=()
     
     background(bgcolor);
     fill(0);
