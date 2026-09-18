864739755b7c88829d06c16e0a0fd0d682828dd5
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-18 09:58:14 +0000

    Sep 18, 2026, 9:58 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,26 +1,32 @@
-let textbox,colorbox;
-let updatetext;
-function setup(){
-    createCanvas(400,200);
-    background("skyblue");
-    textAlign(CENTER,CENTER);
-    textbox=createInput("");
-    textbox.position(100,50)
-    textbox.input(changedText);
 
-    colorbox = createColorPicker("#000")
-    colorbox.position(100,100);
+
+
+
+
+
+// let textbox,colorbox;
+// let updatetext;
+// function setup(){
+//     createCanvas(400,200);
+//     background("skyblue");
+//     textAlign(CENTER,CENTER);
+//     textbox=createInput("");
+//     textbox.position(100,50)
+//     textbox.input(changedText);
+
+//     colorbox = createColorPicker("#000")
+//     colorbox.position(100,100);
     
-}
+// }
 
-function draw(){
-    background("skyblue");
-    fill(colorbox.value());
-    textSize(18);
-    text(updatetext,200,150);
+// function draw(){
+//     background("skyblue");
+//     fill(colorbox.value());
+//     textSize(18);
+//     text(updatetext,200,150);
     
-}
+// }
 
-function changedText(){
-    updatetext = textbox.value();
-}
+// function changedText(){
+//     updatetext = textbox.value();
+// }
