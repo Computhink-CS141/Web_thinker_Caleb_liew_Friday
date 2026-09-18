e239af58b745365a5ff759a0e6af9a2993a2db3b
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-18 09:50:40 +0000

    Sep 18, 2026, 9:50 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,12 +1,12 @@
 let textbox,colorbox;
-let updatetext
+let updatetext;
 function setup(){
     createCanvas(400,200);
     background("skyblue");
     textAlign(CENTER,CENTER);
     textbox=createInput("");
     textbox.position(100,50)
-    textbox.input(updateText);
+    textbox.input(updatetext);
 
     colorbox = createColorPicker("#000")
     colorbox.position(100,100);
