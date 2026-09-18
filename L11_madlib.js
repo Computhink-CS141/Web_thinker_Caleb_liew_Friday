e1cc74c9cd1f8cd78b4d3ff714a69cf1272a0151
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-18 10:44:51 +0000

    Sep 18, 2026, 10:44 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -43,7 +43,10 @@ function setup(){
     ]
     let template = random(storytemplates);
     storytext = template.replace("{noun}", "noun.value()")
-    
+                        .replace("{noun}", "noun.value()")
+                        .replace("{noun}", "noun.value()")
+                        .replace("{noun}", "noun.value()")
+                        .replace("{noun}", "noun.value()")
 
 }
 
