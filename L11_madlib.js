525f698c420051eb36358899bb1a20543d74294b
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-18 10:41:23 +0000

    Sep 18, 2026, 10:41 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -37,8 +37,8 @@ function setup(){
     story.mousePressed(updatetext);
 
     storytemplates = [
-        "The {adj} {noun} decided to {verb} {adv} at the {place}."
-        "The {adj} {noun} decided to {verb} {adv} at the {place}."
+        "The {adj} {noun} decided to {verb} {adv} at the {place}.",
+        "The {adj} {noun} decided to {verb} {ad{place}."
     ]
 
 }
