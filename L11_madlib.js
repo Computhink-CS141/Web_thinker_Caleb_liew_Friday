f36c934f5f16524675b81a4dc21c33c978634af2
Computhink-CS141 <cs141@computhink.com.sg>
2026-09-18 10:30:23 +0000

    Sep 18, 2026, 10:30 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -36,7 +36,9 @@ function setup(){
 
 function draw(){
     background(220);
+    textAlign(RIGHT,CENTER);
     textSize(18);
+
     text("Enter a noun",width/2,25);
     text("Enter a verb",width/2,50);
     text("Enter a adjective",width/2,75);
